37940ba42beb80b8397bcb6e3db96ec66b35754e
Davi Silva <davisilvaphoto@gmail.com>
2025-03-21 20:56:33 -0300

    add components

---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+export const Footer = () => (
+  <footer className="fixed bottom-0 left-0 right-0 flex items-center justify-center bg-zinc-50">
+    <div className="container-wrapper">
+      <div className="container py-4">
+        <div className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Desenvolvido por{' '}
+          <a
+            href="https://www.linkedin.com/in/davisilva85/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            Davi Silva
+          </a>
+          .
+        </div>
+      </div>
+    </div>
+  </footer>
+)
